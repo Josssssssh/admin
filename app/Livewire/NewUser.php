0a71690c573097e43f7b70d7f893ec6a6f97404a
Josssssssh <joshuanava16@gmail.com>
2024-10-27 18:17:09 +0800

    Loading and Buttons

--- app/Livewire/NewUser.php
+++ app/Livewire/NewUser.php
@@ -4,15 +4,45 @@ namespace App\Livewire;
 
 use App\Models\User;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class NewUser extends Component
 {
+    use WithPagination;
+
     public $name;
     public $email;
     public $password; 
 
-    public function submit()
-    {
+    public function testing(){
+
+        sleep(2);
+        $this->validate([
+            'name'=>'required|min:2',
+            'email'=> 'required|email|unique:users,email',
+            'password'=> 'required|min:8',
+        ]);
+   
+        if (User::where('email', $this->email)->exists()) {
+            session()->flash('error', 'Email already exists. Please use a different email.');
+            return;
+        }
+        
+    
+        User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => $this->password,
+        ]);
+
+       
+        $this->reset(['name', 'email', 'password']);
+        session()->flash('message', 'Registration Successful');
+    }
+
+    public function submit(){
+        
+        sleep(2);
         $this->validate([
             'name'=>'required|min:2',
             'email'=> 'required|email|unique:users,email',
@@ -38,8 +68,9 @@ class NewUser extends Component
 
     public function render()
     {
-        return view('livewire.new-user', [
-            'users' => User::all(),
+        return view('livewire.new-user', 
+        [
+            'users' => User::paginate(2),
         ]);
     }
 }
--- resources/views/livewire/new-user.blade.php
+++ resources/views/livewire/new-user.blade.php
@@ -1,3 +1,5 @@
+<div style="margin: 100px;">
+  
 <div>
     <h1 class="text-3xl text-center">USER</h1>
 
@@ -40,12 +42,42 @@
             @enderror
         </div>
 
-        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
-            Register new account
-        </button>
+        
+        <div style="text-align: center;">
+  <button wire:loading.remove type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+    Register
+  </button>
+  
+  <div wire:loading wire:target="submit">
+    <div role="status">
+      <svg aria-hidden="true" class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+      </svg>
+      <span class="sr-only">Loading...</span>
+    </div>
+  </div>
+</div>
+
+<div style="text-align: center; margin-top: 20px;">
+  <button wire:loading.remove type="testing" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+    Testing
+  </button>
+  
+  <div wire:loading wire:target="testing">
+    <div role="status">
+      <svg aria-hidden="true" class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+      </svg>
+      <span class="sr-only">Loading...</span>
+    </div>
+  </div>
+</div>
+</div>
     </form>
 
-    <div class="relative overflow-x-auto shadow-md sm:rounded-lg mb-10 mx-auto">
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg mb-10 mx-10">
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
@@ -56,11 +88,15 @@
             <tbody>
                 @foreach ($users as $user)
                     <tr>
-                        <td class="py-2 px-4 border-t border-gray-200">{{ $user->name }}</td>
-                        <td class="py-2 px-4 border-t border-gray-200">{{ $user->email }}</td>
+                        <td class="py-2 px-5 border-t border-gray-200">{{ $user->name }}</td>
+                        <td class="py-2 px-5 border-t border-gray-200">{{ $user->email }}</td>
                     </tr>
                 @endforeach
             </tbody>
         </table>
     </div>
+    <div>
+        {{ $users->links() }}
+    </div>
+</div>
 </div>
